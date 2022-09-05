aae4c3022586f2d0425e30784dae87107c13cbb9
Murali <muralivvrsn75683@gmail.com>
2022-09-05 15:33:33 +0530

    backSpace Added

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+const buttons = document.querySelectorAll('button');
+const clear = document.getElementById('btn16');
+const answer = document.getElementById('btn15');
+const backSpace = document.getElementById('btn18');
 const input = document.getElementById('enter');
-const array = document.querySelectorAll('.item');
 let str = "";
-for(let i=0;i<array.length;i++){
-    array[i].addEventListener('click',()=>{
-        str+=array[i].textContent;
-        input.value = str;
-    });
+function changeColor(element) {
+    element.style.backgroundColor = 'white';
 }
-
-
+function changeColor1(element){
+    element.style.backgroundColor = 'red';
+    setTimeout(changeColor(element),1000);
+}
+buttons.forEach(element => {
+    element.addEventListener('click', () => {
+        setTimeout(changeColor1(element),1000);
+        if(element!==clear && element!==answer && element!==backSpace){
+            str+=element.textContent;
+        }
+        else if(element===clear){
+            str="";
+        }
+        else if(element===answer){
+            str = eval(str);
+        }
+        else{
+            str=str.slice(0,-1);
+        }
+        input.value = str;
+    })
+});
